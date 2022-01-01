8eb2cb3238d35dccb86333de70a58a3edfe102d2
saad nabilse <sa3d_d97@hotmail.com>
2022-01-01 12:31:49 +0200

    End of section 12

---
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { delay, finalize } from "rxjs/operators";
+import { LoadingService } from "../services/loading.service";
+
+
+@Injectable()
+ export class LoadingInterceptors implements HttpInterceptor{
+     constructor(private busyService:LoadingService){}
+     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+         this.busyService.loading();
+         return next.handle(req).pipe(
+            delay(1000),
+            finalize(()=> {
+                this.busyService.idle();
+            })
+            
+            );
+     }
+    }
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 
-
-
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     SharedModule
-  ],
+  ], 
   exports: [HomeComponent]
 })
 export class HomeModule { }
